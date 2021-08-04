c5560f66c6832b55428113f3e655a345e6379acc
SoftLeaderGy <826044698@qq.com>
2021-08-04 22:58:33 +0800

    将服务消费者80 注册到eureka7001服务中，到此位置单机版eureka搭建完成

--- cloud2020/com.atguigu.springcloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud2020/com.atguigu.springcloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -3,12 +3,14 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Descr iption:
  * @Author: Yang.Guo
  * @Date: 2021/08/04/11:36
  */
+@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class OrderMain80 {
     public static void main(String[] args) {
