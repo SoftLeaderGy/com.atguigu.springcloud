f40551eb330d3a7213f3b17cacc86c006e09711f
SoftLeaderGy <826044698@qq.com>
2021-08-06 10:40:10 +0800

    RestTemplate的二次使用，获取entity。 Ribbon = 负载均衡+RestTemplate调用

--- cloud2020/com.atguigu.springcloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud2020/com.atguigu.springcloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,23 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+
+    /**
+     * 返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等。
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getFotEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable Long id){
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
     }
 
 }
