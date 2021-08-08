fdb2d658f531172ea57583960fd76a897b2a930f
SoftLeaderGy <826044698@qq.com>
2021-08-08 20:18:53 +0800

    创建网关服务，并配置provider-payment8001的服务路由

---
+++ cloud2020/com.atguigu.springcloud/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: Yang.Guo
+ * @Date: 2021/08/08/19:13
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
